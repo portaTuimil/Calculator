f469af30385e4f9366449dc39b53ad3bb52a8879
Pablo Porta <pabloportatuimil12@gmail.com>
2023-02-16 22:24:35 +0100

    Calculadora operable

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let operator = '';
 let secondValue = '';
 let result = '';
 let screen = '';
+let counter = 1;
 
 
 
@@ -12,16 +13,82 @@ let calcScreen = document.querySelector('#screen');
 let reScreen = document.querySelector('#result')
 
 button.forEach(button =>{button.addEventListener('click', (event)=>{
+    let id = button.getAttribute('id');
+    isNaN(parseInt(id))
+
+    switch (id){
+        case 'AC':
+            screen = '';
+            firstValue = '';
+            secondValue = '';
+            operator = '';
+            counter = 1;
+            result = '';
+            break
+
+        case 'C':
+            screen = screen.substring(0, screen.length - 1)
+            if (counter = 1 && operator ==''){
+                firstValue = firstValue.substring(0, firstValue.length - 1)
+            } else if (counter = 1 && operator !==''){
+                operator = '';
+                counter = 1;
+            }else{
+                secondValue = secondValue.substring(0, secondValue.length - 1)
+            }
+            break
+
+        case 'π':
+            if (counter = 1);
 
 
+        default:
+            if (counter == 1){
+                if(isNaN(parseInt(id)) == false){
+                    firstValue += id
+                    screen += id;
+                } else{
+                    operator = id;
+                    counter = 2;
+                    screen += id;
+                }
 
+            } else if (counter = 2){
+                if (isNaN(parseInt(id)) == false){
+                    secondValue += id;
+                    screen += id;
 
+                } else{
+                    if (id == '='){
+                        switch (operator){
+                            case '+':
+                                result = parseInt(firstValue)+ parseInt(secondValue)
+                                break
+                            case '-':
+                                result = parseInt(firstValue)- parseInt(secondValue)
+                                break
+                            case '*':
+                                result = parseInt(firstValue)* parseInt(secondValue)
+                                break
+                            case '/':
+                                result = parseInt(firstValue)/ parseInt(secondValue)
+                                break
+                            case '^':
+                                result = parseInt(firstValue)** parseInt(secondValue)
+                                break
+                            case '%':
+                                result = parseInt(firstValue)% parseInt(secondValue)
+                                break
+                        }
+                        reScreen.textContent = result;
+                    } else{
+                        alert('Introduce a number')
+                    }
+                }
 
+            }
 
-
-    //Screen:
-    screen += button.getAttribute('id');
+    }
     calcScreen.textContent = screen;
     reScreen.textContent = result;
-    alert(screen)
 })})
