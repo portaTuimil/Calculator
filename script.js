2a9c6f0affa6a02282b9621a769daaeb64f0976b
Pablo Porta <pabloportatuimil12@gmail.com>
2023-02-17 15:25:40 +0100

    First stable version

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Lord, forgive me for what I`m about to write
 let firstValue = '';
 let operator = '';
 let secondValue = '';
@@ -14,81 +15,70 @@ let reScreen = document.querySelector('#result')
 
 button.forEach(button =>{button.addEventListener('click', (event)=>{
     let id = button.getAttribute('id');
-    isNaN(parseInt(id))
+    //Counter Actualizer
+    if (operator == ''){
+        counter = 1;
+    } else{
+        counter = 2;
+    }
 
     switch (id){
         case 'AC':
-            screen = '';
             firstValue = '';
             secondValue = '';
             operator = '';
             counter = 1;
             result = '';
             break
-
+    
         case 'C':
-            screen = screen.substring(0, screen.length - 1)
-            if (counter = 1 && operator ==''){
-                firstValue = firstValue.substring(0, firstValue.length - 1)
-            } else if (counter = 1 && operator !==''){
-                operator = '';
-                counter = 1;
-            }else{
+            if (secondValue !== ''){
                 secondValue = secondValue.substring(0, secondValue.length - 1)
+            } else if (operator !== ''){
+                operator = '';
+            } else if (firstValue !== ''){
+                firstValue = firstValue.substring(0, firstValue.length - 1)
             }
             break
-
-        case 'π':
-            if (counter = 1);
-
-
+    
         default:
-            if (counter == 1){
-                if(isNaN(parseInt(id)) == false){
-                    firstValue += id
-                    screen += id;
-                } else{
-                    operator = id;
-                    counter = 2;
-                    screen += id;
-                }
-
-            } else if (counter = 2){
-                if (isNaN(parseInt(id)) == false){
-                    secondValue += id;
-                    screen += id;
-
-                } else{
-                    if (id == '='){
-                        switch (operator){
-                            case '+':
-                                result = parseInt(firstValue)+ parseInt(secondValue)
-                                break
-                            case '-':
-                                result = parseInt(firstValue)- parseInt(secondValue)
-                                break
-                            case '*':
-                                result = parseInt(firstValue)* parseInt(secondValue)
-                                break
-                            case '/':
-                                result = parseInt(firstValue)/ parseInt(secondValue)
-                                break
-                            case '^':
-                                result = parseInt(firstValue)** parseInt(secondValue)
-                                break
-                            case '%':
-                                result = parseInt(firstValue)% parseInt(secondValue)
-                                break
-                        }
-                        reScreen.textContent = result;
-                    } else{
-                        alert('Introduce a number')
-                    }
-                }
-
+            if (isNaN(parseInt(id)) == false && operator ==''){
+                firstValue += id;
+            } else if (isNaN(parseInt(id)) == false && operator !==''){
+                secondValue+= id;
+            } else if (isNaN(parseInt(id)) == true && operator ==''){
+                operator +=id;
             }
+            else{
+                alert('Thats not posible')
+            }
+        }
 
+    //Pantalla
+    switch (operator){
+         case '+':
+            result = parseInt(firstValue) + parseInt(secondValue)
+            break
+        case '-':
+            result = parseInt(firstValue) - parseInt(secondValue)
+            break
+        case '*':
+            result = parseInt(firstValue) * parseInt(secondValue)
+            break
+        case '/':
+            result = parseInt(firstValue) / parseInt(secondValue)
+            break
+        case '^':
+            result = parseInt(firstValue) ** parseInt(secondValue)
+            break
+        case '%':
+            result = parseInt(firstValue) % parseInt(secondValue)
+            break
     }
+    if (isNaN(result)){
+        result =''
+    }
+    screen = firstValue+operator+secondValue;
     calcScreen.textContent = screen;
     reScreen.textContent = result;
 })})
